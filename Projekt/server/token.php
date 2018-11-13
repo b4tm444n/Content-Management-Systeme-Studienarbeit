13d1984accb13123f3488933c78a0c788f7bad92
cBrownOTH <c.braun1@oth-aw.de>
2018-11-13 18:55:20 +0100

    Implementierung Projektdatenfunktionen/Allgemeine Optimierungen
    token.php: Dem Token wird nun zusätzlich die NutzerID hinzugefügt;
    projectFuncs.php: Beinhaltet die Funktionen zur Abfrage der Datenbank von Projektinformationen;
    projectRouter.php: Routet bei Anfrage des Frontends auf die entsprechende Funktion in projectFuncs.php;
    contentlibrary.js: Anfrage für alle Projekte und Anfrage für Projekte bei denen der Nutzer Projektleiter ist implementiert;
    Usersite.html: Projekte laden(Button) ruft nun alle Projekte aus der Datenbank ab, Meine Projekte(Button) ruft alle Projekte bei denen der Nutzer Projektleiter ist ab;

--- Projekt/server/token.php
+++ Projekt/server/token.php
@@ -5,18 +5,18 @@ session_start();
 //Verbindung herstellen
 $db = dbsConnect();
 //Nach Eintrag des Nutzers suchen
-$user = dbsSingleRow($db, "SELECT Passwort AS pw, Admin FROM nutzer WHERE Username='".$_POST['name']."'");
+$user = dbsSingleRow($db, "SELECT Passwort AS pw, Admin, NutzerID AS id FROM nutzer WHERE Username='".$_POST['name']."'");
 //User authentifizieren
 if($user != null && $user['pw'] == $_POST['pw'])
 {
   if($user['Admin'])
   {
-    $_SESSION['token'] = "isAdmin" . "," . time();
+    $_SESSION['token'] = "isAdmin" . "," . time() . "," . $user['id'];
     $data = array('status' => true, 'type' => "admin");
   }
   else
   {
-    $_SESSION['token'] = "isUser" . "," . time();
+    $_SESSION['token'] = "isUser" . "," . time() . "," . $user['id'];
     $data = array('status' => true, 'type' => "user");
   }
 }
